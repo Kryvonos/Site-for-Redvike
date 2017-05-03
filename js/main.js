83cc687c060bc808d380b6ae6338cb7642f16bcf
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2017-05-03 21:21:07 +0300

    Included fonts, created menu burger

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
+'use strict';
+
 ;(function( $ ) {
+	var $menuBurger = $('#menuBurger'),
+			menuBurgerClickTimeoutId = null;
 
 	function init() {
   		if ( ! Modernizr.flexbox ) {
   			window.location = "./browser-update.html";
   		}
+
+			initEventListeners();
 	};
+
+	function initEventListeners() {
+		$menuBurger.click( onMenuBurgerClick );
+	}
+
+	function onMenuBurgerClick( event ) {
+		var clickDelay = 500;
+
+		if ( menuBurgerClickTimeoutId === null ) {
+
+			if ( ! $menuBurger.hasClass('active') ) {
+				$menuBurger.addClass('active');
+			}
+			else {
+				$menuBurger.addClass('closing');
+			}
+
+			menuBurgerClickTimeoutId = setTimeout( function() {
+				menuBurgerClickTimeoutId = null;
+
+				if ( $menuBurger.hasClass('closing') ) {
+					$menuBurger.removeClass('active closing');
+				}
+			}, clickDelay );
+		}
+	}
 
   init();
 
